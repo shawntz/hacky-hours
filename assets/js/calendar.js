11249fcb0e1a4a5938d03b5e169ca00fbc7e5287
shawn schwartz <shawn.t.schwartz@gmail.com>
2025-04-28 15:41:13 -0700

    Update calendar.js

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -1,5 +1,5 @@
 const calendarContainer = document.getElementById('calendar');
-const startDate = new Date("2025-03-30"); // First cell of the calendar (make it a Sunday even though Monday is the first day shown)
+const startDate = new Date("2025-04-27"); // First cell of the calendar (make it a Sunday even though Monday is the first day shown)
 const endDate = new Date(startDate);
 endDate.setDate(startDate.getDate() + 27); // Four weeks from now
 
